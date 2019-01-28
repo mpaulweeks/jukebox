d1a7bbc7211f6b83faac4a2cd752d3ce7fe721cb
mpaulweeks <mpaulweeks@gmail.com>
2019-01-28 14:40:29 -0800

    do uploads synchronously

--- packages/loader/src/loader.ts
+++ packages/loader/src/loader.ts
@@ -82,19 +82,28 @@ export class Loader {
     }, {});
   }
 
+  async asyncMap<E, T>(array: Array<E>, callback: (elm: E, index: number, array: Array<E>) => Promise<T>): Promise<Array<T>> {
+    // https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
+    const output: Array<T> = [];
+    for (let index = 0; index < array.length; index++) {
+      const result = await callback(array[index], index, array);
+      output.push(result);
+    }
+    return output;
+  }
+
   async export() {
+    Logger.log('begin exporting...');
     const { store, collection, infoLookup, toUploadAudio, toUploadImage } = this;
 
     await store.uploadData(Constants.CollectionFileName, collection.data);
     await store.uploadData(Constants.InfoLookupFileName, infoLookup.data);
 
-    const trackPromises = toUploadAudio.map(track => store.uploadAudio(track.id, track.path));
+    await this.asyncMap(toUploadAudio, (track => store.uploadAudio(track.id, track.path)));
     this.toUploadAudio = [];
-    await Promise.all(trackPromises);
 
-    const imagePromises = toUploadImage.map(image => store.uploadImage(image.hash, image.buffer));
+    await this.asyncMap(toUploadImage, (image => store.uploadImage(image.hash, image.buffer)));
     this.toUploadImage = [];
-    await Promise.all(imagePromises);
 
     Logger.log('success!')
   }
