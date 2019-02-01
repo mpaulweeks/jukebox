492fe50cc8485d6b17d1c5d51f51e131a26d075d
mpaulweeks <mpaulweeks@gmail.com>
2019-02-01 09:52:40 -0500

    Updates

---
+++ precache-manifest.f962fb15d64c0d4524af2269e049fe68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "415844c1425eb946acba",
+    "url": "./static/js/main.415844c1.chunk.js"
+  },
+  {
+    "revision": "6624a1173cd6287ce7c3",
+    "url": "./static/js/1.6624a117.chunk.js"
+  },
+  {
+    "revision": "415844c1425eb946acba",
+    "url": "./static/css/main.9aa89945.chunk.css"
+  },
+  {
+    "revision": "c336b26ac9cef4a5c52c75fcc0916fb2",
+    "url": "./index.html"
+  }
+];
